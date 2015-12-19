bbf4f080496a471037913cb885592548bd4e383e
root <root@clm-pun-015875.bmc.com>
2015-12-19 06:34:04 -0500

    location update complete

--- server/api/routes/ServiceCurrentLocation.js
+++ server/api/routes/ServiceCurrentLocation.js
@@ -1,12 +1,28 @@
 /*
- * Project Elll  Current Location service.
+ * Project Elll Emergency Contacts service.
  */
 
 exports.currentLocationImpl = function (req, res)
 {
-        var mobile = req.body.mobile;
+        var mobile = req.body.authtoken;
+        var cassandra = require('cassandra-driver');
+        var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'elll'})
+	
+	var latlong = req.body.latlong;
+	var user = {"mobile": mobile, "latlong": latlong};
+        var query = "UPDATE user SET latlong = :latlong where mobile = :mobile";
+ 
+        client.execute(query, user, { prepare: true}, function (err, result) {
+           // Run next function in series
+        	if (err) {
+	            console.log(err);
+	            res.status(500).send({"error": "Unable to update current location"});
+	        }
 
-        console.log(mobile);
-        res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-        res.end("Successful");
+        	if (result) {
+	            res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+	            res.end();
+	        }
+        });
 };
+
