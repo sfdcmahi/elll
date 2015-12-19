a7580cde8b4d0c524d466c4a75160e1c7580d547
root <root@clm-pun-015875.bmc.com>
2015-12-19 04:56:31 -0500

    user data with solr

--- server/datastore/solr/dummy_data.js
+++ server/datastore/solr/dummy_data.js
@@ -1,8 +1,9 @@
 var cassandra = require('cassandra-driver');
 var id = cassandra.types.Uuid.random().toString();
+console.log(">>>>>>>"+id);
 var mobile = '9008600881';
 var email = 'gokumar@bmc.com';
-var latlong = '12.967808642028006, 77.60687189419559';
+var latlong = '36.03403034, -79.60687189419559';
 
 var ec = []
 var emergency = {"mobile": "9242263584"}
@@ -13,7 +14,7 @@ ec.push(emergency);
 
 var user = {"mobile": mobile, "id": id, "email":email, "latlong":latlong, "emergencycontacts":ec};
 
-var query = 'INSERT INTO user (id, mobile, email, latlong, emergencycontacts) VALUES (:id, :mobile, :email, :latlong, :emergencycontacts)';
+var query = 'INSERT INTO user (userid, mobile, email, latlong) VALUES (:id, :mobile, :email, :latlong)';
 
 var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'elll'});
 client.execute(query, user, { prepare: true }, function(error, result){
